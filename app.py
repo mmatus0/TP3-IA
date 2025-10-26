0023087d5d625024d175021fc8127c9ab96daec7
mmatus0 <gonzalo.matus2201@alumnos.ubiobio.cl>
2025-10-26 19:57:05 -0300

    Integración de BDD y Flask

--- app.py
+++ app.py
@@ -62,13 +62,96 @@ def inferir_riesgo(datos_entrada):
                 "accion": regla["accion"],
                 "justificacion": f"Se activó la regla ID {regla['id']}: {regla['nombre']}."
             }
-            
+
+             
     # Por defecto
     return {
         "nivel": "NO CLASIFICADO",
         "accion": "El nivel de riesgo no se ajusta a las reglas existentes. Mantener monitoreo.",
         "justificacion": "Ninguna regla de la Base de Conocimiento cumplió todas las condiciones."
-    }
+    } 
+
+
+  # ----- Base de Datos - SQLite ----- 
+
+
+def init_db():
+    """Inicializa la BDD para almacenar el historial de riesgos""" 
+    with sqlite3.connect(DB_NAME) as connexion: 
+        cursor = connexion.cursor()
+        cursor.execute(''' 
+        CREATE TABLE IF NOT EXISTS diagnosticos( 
+            id INTEGER PRIMARY KEY AITOINCREMENT,
+            fecha TEXT,
+            temperatura REAL,
+            humedad REAL, 
+            viento REAL, 
+            nivel_riesgo TEXT,
+            justificacion TEXT
+        )
+        ''') 
+        connexion.commit()
+
+
+def guardar_diagnostico(datos_entrada, resultado):
+    """Almacena el riesgo y los hechos en la BDD"""
+    with sqlite3.connect(DB_NAME) as connexion: 
+        cursor = connexion.cursor()
+        cursor.execute('''
+        INSERT INTO diagnosticos
+            (fecha, temperatura, humedad, viento, nivel_riesgo, justificacion)
+        VALUES (?, ?, ?, ?, ?, ?)
+        ''', (
+            datetime.now().isoformat(),
+            datos_entrada.get('temperatura'),
+            datos_entrada.get('humedad'),
+            datos_entrada.get('viento'),
+            resultado['nivel'],
+            resultado['justificacion']
+        )) 
+        connexion.commit()
+                       
+def obtener_diagnosticos():
+    """Obtiene el historial de riesgos (limitado a 10 registros recientes)."""
+    with sqlite3.connect(DB_NAME) as connexion:
+        cursor = connexion.cursor()
+        cursor.execute("SELECT fecha, temperatura, humedad, viento, nivel_riesgo FROM diagnosticos ORDER BY id DESC LIMIT 10")
+        return cursor.fetchall()  
+
+
+
+# ------ Flask -----
+
+app.route("/", methods=["GET", "POST"])
+def index():
+    cargar_reglas()
+    resultado = None
+    datos_entrada_display = {}
+    registros = obtener_diagnosticos() # Carga el historial al abrir la página
+
+    if request.method == "POST":
+        # Recolectar hechos (datos de entrada)
+        datos_entrada = {
+            'temperatura': request.form.get('temperatura'),
+            'humedad': request.form.get('humedad'),
+            'viento': request.form.get('viento')
+        }
+       
+        datos_entrada_display = datos_entrada
+       
+        # Llamar al Motor de Inferencia
+        resultado = inferir_riesgo(datos_entrada)
+       
+        # Guardar el nuevo riesgo y recargar el historial
+        guardar_diagnostico(datos_entrada, resultado)
+        registros = obtener_diagnosticos()
+
+    # Renderizar la plantilla HTML
+    return render_template("index.html",
+                           resultado=resultado,
+                           datos_entrada=datos_entrada_display,
+                           registros=registros)
+
 
 # --- Ejecución del Servidor ---
 if __name__ == "__main__":
